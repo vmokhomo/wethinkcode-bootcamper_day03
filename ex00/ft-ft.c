593ecdd7e68906c8afa19e4d8331d11edaa83ea8
Victor Mokhomong <vmokhomo@c4r7s3.wethinkcode.co.za>
2020-06-19 15:15:31 +0200

    day03/ex00

---
+++ ex00/ft-ft.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+//#include <stdlib.h>
+
+void ft_putchar(char c);
+/*{
+	write(1, &c, 1);
+}*/
+void ft_ft(int *nbr)
+{
+	int i;
+
+	i = 42;
+	nbr = &i;
+}
+/*int main()
+{
+	ft_ft();
+}*/
