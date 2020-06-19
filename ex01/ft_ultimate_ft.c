5267d5c94bf408baa27a8eb7c7d3a05c4f9abbbe
Victor Mokhomong <vmokhomo@c4r7s3.wethinkcode.co.za>
2020-06-19 15:16:30 +0200

    day03.

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,25 @@
+void ft_ultimate_ft(int **********nbr)
+{
+	int *********a;
+	int ********b:
+	int *******c;
+	int ******d;
+	int *****e;
+	int ****f;
+	int ***g;
+	int **h;
+	int *i;
+	int j;
+
+	int j = 42;
+	nbr = &a;
+	a = &b;
+	b = &c;
+	c = &d;
+	d = &e;
+	e = &f;
+	f = &g;
+	g = &h;
+	h = &i;
+	i = &j;
+}
