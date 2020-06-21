5ad988b1915eb7eb6e0e5eee30c18d2e9bb05560
Victor Mokhomong <vmokhomo@c4r7s3.wethinkcode.co.za>
2020-06-21 10:26:55 +0200

    day03/ex04

--- ex04/ft_ultimate_div_mod.c
+++ ex04/ft_ultimate_div_mod.c
@@ -1,9 +1,23 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmokhomo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/21 10:09:22 by vmokhomo          #+#    #+#             */
+/*   Updated: 2020/06/21 10:09:29 by vmokhomo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void ft_putchar(char c);
-/*{
-	write(1, &c, 1);
-}*/
+
+/* #include <unistd.h>
+**
+**	void ft_putchar(char c);
+**	{
+**		write(1, &c, 1);
+**	}
+*/
 void ft_ultimate_div_mod(int *a, int *b)
 {
 	int diff;
@@ -11,15 +25,12 @@ void ft_ultimate_div_mod(int *a, int *b)
 	diff = *a;
 	*a = (*a)/(*b);
 	*b = (diff)%(*b);
-
-	//ft_putchar(*a + '0');
-	//ft_putchar(',');
-	//ft_putchar(*b + '0');
 }
-/*int main()
-{
-	int a = 2;
-	int b = 3;
-
-	ft_ultimate_div_mod(&a, &b);
-}*/
+/*	int main()
+**	{
+**		int a = 2;
+**		int b = 3;
+**
+**		ft_ultimate_div_mod(&a, &b);
+**	}
+*/
