3417a27c4c727c7edfabbbd2e787cd7f4a39aea7
Victor Mokhomong <vmokhomo@c4r7s3.wethinkcode.co.za>
2020-06-21 10:25:07 +0200

    day03/ex03

--- ex03/ft_div_mod.c
+++ ex03/ft_div_mod.c
@@ -1,9 +1,10 @@
 #include <unistd.h>
 
-void ft_putchar(char c);
-/*{
-	write(1, &c, 1);
-}*/
+/*	void ft_putchar(char c);
+**	{
+**		write(1, &c, 1);
+**	}
+*/
 void ft_div_mod(int a, int b, int *div, int *mod)
 {
 	*div = a/b;
@@ -12,12 +13,13 @@ void ft_div_mod(int a, int b, int *div, int *mod)
 	ft_putchar(*div + '0');
 	ft_putchar(*mod + '0');
 }
-/*int main()
-{
-	int a = 2;
-	int b = 3;
-	int c = 0;
-	int d = 0;
-
-	ft_div_mod(a, b, &c, &d);
-}*/
+/*	int main()
+**	{
+**		int a = 2;
+**		int b = 3;
+**		int c = 0;
+**		int d = 0;
+**
+**		ft_div_mod(a, b, &c, &d);
+**	}
+*/
